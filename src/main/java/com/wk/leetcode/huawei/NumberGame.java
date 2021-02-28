fe2f63e5ae72dac38b49b897c7f0f447d6815ff7
wongkin <wkhxj@foxmail.com>
2021-02-28 21:36:55 +0800

    华为机试

---
+++ src/main/java/com/wk/leetcode/huawei/NumberGame.java
@@ -0,0 +1,47 @@
+package com.wk.leetcode.huawei;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 华为机试
+ * 100个人围成一圈，从1开始报数，报到M的人出局，输出剩下的M-1个人的编号
+ * 牛客网根据输出的结果判断用例是否通过，提交后不可修改，淦
+ */
+public class NumberGame {
+    public static void main(String[] args) {
+        List<Integer> r = move(3);
+        StringBuffer sb = new StringBuffer();
+        for (int i=0; i<r.size();i++) {
+            sb.append(r.get(i));
+            if (i+1 != r.size()) {
+                sb.append(",");
+            }
+        }
+        System.out.println(sb.toString());
+        System.exit(0);
+    }
+
+    public static List<Integer> move(int num) {
+        List<Integer> nums = new ArrayList<>();
+        for (int i=1;i<=100;i++) {
+            nums.add(i);
+        }
+
+        int i=1;
+        while (nums.size() >= num) {
+            List<Integer> tmp = new ArrayList<>();
+            for (int j=0;j<nums.size();j++) {
+                if (i != num) {
+                    tmp.add(nums.get(j));
+                    i++;
+                } else {
+                    i = 1;
+                }
+            }
+            nums = tmp;
+        }
+        return nums;
+    }
+
+}
